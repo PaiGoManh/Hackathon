5125a1ec4809da41e87960f210942cad278fffb4
anandhuRaveendran <anandhuraveendrank@gmail.com>
2024-09-22 10:00:32 +0530

    request

--- ui/src/pages/Prover/Requests.jsx
+++ ui/src/pages/Prover/Requests.jsx
@@ -1,7 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import { MdOutlineContentPasteGo } from "react-icons/md";
+
+const data = [
+  { ID: '001', Condition: 'age', Status: 'Active' },
+  { ID: '002', Condition: 'age', Status: 'Inactive' },
+
+];
 
 const Requests = () => {
+
   return (
     <div className='pt-[5%]'>
         <div className='text-center lg:mt-0 mt-[10%] text-2xl font-bold '>REQUESTS PENDING</div>
@@ -16,7 +24,18 @@ const Requests = () => {
                 </tr>
             </thead>
             <tbody>
-
+              {data.map((item) => (
+              <tr key={item.ID} className="border-t border-gray-300 text-black">
+                <td className="px-4 py-2 text-center">{item.ID}</td>
+                <td className="px-4 py-2 text-center">{item.Condition}</td>
+                <td className="px-4 py-2 text-center">{item.Status}</td>
+                <td className="px-4 py-2 text-center">
+                  <a href={item.View} className="text-blue-500 hover:underline">
+                    <MdOutlineContentPasteGo/>
+                  </a>
+                </td>
+              </tr>
+              ))}
             </tbody>
             </table>
 
