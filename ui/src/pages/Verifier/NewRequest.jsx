91943affc1cdfde36e1b440992ade0ea7b9dd9ed
anandhuRaveendran <anandhuraveendrank@gmail.com>
2024-09-22 13:24:04 +0530

    merge conflit solved

--- ui/src/pages/Verifier/NewRequest.jsx
+++ ui/src/pages/Verifier/NewRequest.jsx
@@ -1,164 +1,123 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState ,useEffect} from 'react';
 import { BrowserProvider,Contract } from 'ethers';
-const contractABI = 
-  [
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "_requestId",
-          "type": "uint256"
-        },
-        {
-          "internalType": "bool",
-          "name": "_status",
-          "type": "bool"
-        }
-      ],
-      "name": "completeVerification",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "_prover",
-          "type": "address"
-        }
-      ],
-      "name": "requestVerification",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": true,
-          "internalType": "address",
-          "name": "prover",
-          "type": "address"
-        },
-        {
-          "indexed": false,
-          "internalType": "uint256",
-          "name": "requestId",
-          "type": "uint256"
-        },
-        {
-          "indexed": false,
-          "internalType": "bool",
-          "name": "status",
-          "type": "bool"
-        }
-      ],
-      "name": "VerificationCompleted",
-      "type": "event"
-    },
-    {
-      "anonymous": false,
-      "inputs": [
-        {
-          "indexed": true,
-          "internalType": "address",
-          "name": "verifier",
-          "type": "address"
-        },
-        {
-          "indexed": true,
-          "internalType": "address",
-          "name": "prover",
-          "type": "address"
-        },
-        {
-          "indexed": false,
-          "internalType": "uint256",
-          "name": "requestId",
-          "type": "uint256"
-        }
-      ],
-      "name": "VerificationRequested",
-      "type": "event"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "_requestId",
-          "type": "uint256"
-        }
-      ],
-      "name": "getVerificationStatus",
-      "outputs": [
-        {
-          "internalType": "bool",
-          "name": "",
-          "type": "bool"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "requestIdCounter",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "name": "verifications",
-      "outputs": [
-        {
-          "internalType": "address",
-          "name": "verifier",
-          "type": "address"
-        },
-        {
-          "internalType": "address",
-          "name": "prover",
-          "type": "address"
-        },
-        {
-          "internalType": "bool",
-          "name": "completed",
-          "type": "bool"
-        },
-        {
-          "internalType": "bool",
-          "name": "status",
-          "type": "bool"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    }
-  ]
-
-const contractAddress = '0xf63F2dc5634fAF75Fb9227B0ac0f5132BBE6A35c';
+const contractABI = [
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "verifier",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "prover",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "bool",
+				"name": "status",
+				"type": "bool"
+			}
+		],
+		"name": "DataStored",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_prover",
+				"type": "address"
+			},
+			{
+				"internalType": "bool",
+				"name": "_status",
+				"type": "bool"
+			}
+		],
+		"name": "storeData",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getTotalVerifications",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "index",
+				"type": "uint256"
+			}
+		],
+		"name": "getVerification",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			},
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "verifications",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "verifier",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "prover",
+				"type": "address"
+			},
+			{
+				"internalType": "bool",
+				"name": "status",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+const contractAddress = '0x235bAb1c04159Fb5d11b417E34Ef8d6Ab6e833d7';
 
 
 
@@ -167,6 +126,7 @@ const [contract, setContract] = useState(null);
 const [proverAddress, setProverAddress] = useState('');
   const [selectedOption, setSelectedOption] = useState('');
   const [account, setAccount] = useState(null);
+    const [zkpStatus, setZkpStatus] = useState(false);
 
   const [minAge,setMinage]=useState('');
   const [maxAge,setMaxage]=useState('');
@@ -191,16 +151,38 @@ const [proverAddress, setProverAddress] = useState('');
   
     initBlockchain();
   }, []);
-  const handleRequestVerification = async () => {
+//   const handleRequestVerification = async () => {
+//     try {
+//       console.log(proverAddress)
+//       const tx = await contract.requestVerification(proverAddress);
+//       const receipt = await tx.wait();
+//       console.log(receipt)
+//       const verificationRequestedEventSignature = ethers.utils.id('VerificationRequested(uint256,address)');
+
+//       const verificationRequestedEvent = receipt.logs.find(log => {
+//         return log.topics[0] === verificationRequestedEventSignature;
+//     });
+
+//     if (verificationRequestedEvent) {
+//         // The data field is in hex format; we need to convert it to a number
+//         const requestId = ethers.BigNumber.from(verificationRequestedEvent.data).toNumber();
+//         setRequestId(requestId);
+//         console.log('Verification request sent:', requestId);
+//     } else {
+//         console.error('VerificationRequested event not found in the receipt');
+//     }
+// } catch (error) {
+//     console.error('Error requesting verification:', error);
+//       console.log('Verification request sent:', requestId);
+//     } 
+  //   };
+  const handleStoreData = async () => {
     try {
-      console.log(proverAddress)
-      const tx = await contract.requestVerification(proverAddress);
-      const receipt = await tx.wait();
-      const event = receipt.events.find(event => event.event === 'VerificationRequested');
-      const requestId = event.args.requestId.toNumber();
-      console.log('Verification request sent:', requestId);
+      const tx = await contract.storeData(proverAddress, zkpStatus);
+      await tx.wait();
+      console.log('Data stored successfully');
     } catch (error) {
-      console.error('Error requesting verification:', error);
+      console.error('Error storing data:', error);
     }
   };
   const handleSelectChange = (e) => {
@@ -210,7 +192,8 @@ const [proverAddress, setProverAddress] = useState('');
   };
 const handleSubmit = (e)=>{
   e.preventDefault();
-handleRequestVerification()
+  setZkpStatus('successfully requested')
+  handleStoreData()
 localStorage.setItem('minAge',minAge);
 localStorage.setItem('maxAge',maxAge);
 localStorage.setItem('category',selectedOption);
