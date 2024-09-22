151395ac1fad57834c3fdefe5e04e285223fdcbf
anandhuRaveendran <anandhuraveendrank@gmail.com>
2024-09-22 12:18:44 +0530

    contract added

--- ui/src/pages/Prover/ProverHome.jsx
+++ ui/src/pages/Prover/ProverHome.jsx
@@ -1,14 +1,206 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { BrowserProvider,Contract } from 'ethers';
+const contractABI = 
+  [
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_requestId",
+          "type": "uint256"
+        },
+        {
+          "internalType": "bool",
+          "name": "_status",
+          "type": "bool"
+        }
+      ],
+      "name": "completeVerification",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_prover",
+          "type": "address"
+        }
+      ],
+      "name": "requestVerification",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "prover",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "requestId",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "bool",
+          "name": "status",
+          "type": "bool"
+        }
+      ],
+      "name": "VerificationCompleted",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "verifier",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "prover",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "requestId",
+          "type": "uint256"
+        }
+      ],
+      "name": "VerificationRequested",
+      "type": "event"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_requestId",
+          "type": "uint256"
+        }
+      ],
+      "name": "getVerificationStatus",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "requestIdCounter",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "verifications",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "verifier",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "prover",
+          "type": "address"
+        },
+        {
+          "internalType": "bool",
+          "name": "completed",
+          "type": "bool"
+        },
+        {
+          "internalType": "bool",
+          "name": "status",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ]
 
+const contractAddress = '0xf63F2dc5634fAF75Fb9227B0ac0f5132BBE6A35c';
 const ProverHome = () => {
     const [inputValue, setInputValue] = useState('');
 
-
+    const [contract, setContract] = useState(null);
+    const [account, setAccount] = useState(null);
+    const [requestId, setRequestId] = useState('');
+    const [zkpStatus, setZkpStatus] = useState(false);
+  
+    useEffect(() => {
+        const initBlockchain = async () => {
+            if (window.ethereum) {
+              const provider = new BrowserProvider(window.ethereum);
+        
+              // Request account access
+              const signer = await provider.getSigner();
+              console.log('address',signer.address)
+      
+          const contract = new Contract(contractAddress, contractABI, signer);
+          setContract(contract);
+  
+          const userAddress = await signer.getAddress();
+          setAccount(userAddress);
+        } else {
+          console.log('Ethereum wallet not found');
+        }
+      };
+  
+      initBlockchain();
+    }, []);
+    const handleCompleteVerification = async () => {
+        try {
+          const tx = await contract.completeVerification(requestId, zkpStatus);
+          await tx.wait();
+          console.log('Verification completed with status:', zkpStatus);
+        } catch (error) {
+          console.error('Error completing verification:', error);
+        }
+      };
     const handleSubmit = async (e) => {
         e.preventDefault();
-
+        handleCompleteVerification()
         const maxAge = localStorage.getItem('maxAge');
         const minAge = localStorage.getItem('minAge');
         const data = { inputValue, maxAge, minAge };
@@ -24,8 +216,10 @@ const ProverHome = () => {
         const result = await response.json();
 
         if (response.ok) {
+            setZkpStatus('success')
             toast.success(result.message); 
         } else {
+            setZkpStatus('failed')
             toast.error(result.message); 
         }
     };
@@ -34,6 +228,8 @@ const ProverHome = () => {
         <div className='pt-[10%] lg:pt-[5%] lg:ml-[35%] md:ml-[30%] sm:ml-[20%] ml-5'>
             <ToastContainer /> 
             <div className='lg:w-[400px] lg:h-[400px] border-[3px] border-[white] rounded-md w-[340px] h-[470px]'>
+            <p>Connected Account: {account}</p>
+
                 <form className='flex flex-col gap-4 text-black lg:ml-[20%] ml-[15%] mt-[10%]' onSubmit={handleSubmit}>
                     <button
                         type="button" 
@@ -41,6 +237,17 @@ const ProverHome = () => {
                     >
                         Connect DigiLocker
                     </button>
+                    <div className='mt-[5%]'>
+                        <label className="block text-sm font-medium text-white">Enter Request ID</label>
+                        <input
+                            id="textInput"
+                            type="text"
+                            placeholder="Enter your text here"
+                            value={requestId}
+                            onChange={(e) => setRequestId(e.target.value)}
+                            className="mt-1 block w-[250px] p-2 bg-gray-50 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+                        />
+                    </div>
                     <div className='mt-[5%]'>
                         <label className="block text-sm font-medium text-white">Enter type</label>
                         <input
--- ui/src/pages/Verifier/NewRequest.jsx
+++ ui/src/pages/Verifier/NewRequest.jsx
@@ -1,11 +1,208 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useState } from 'react';
+import React, { useState ,useEffect} from 'react';
+import { BrowserProvider,Contract } from 'ethers';
+const contractABI = 
+  [
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_requestId",
+          "type": "uint256"
+        },
+        {
+          "internalType": "bool",
+          "name": "_status",
+          "type": "bool"
+        }
+      ],
+      "name": "completeVerification",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_prover",
+          "type": "address"
+        }
+      ],
+      "name": "requestVerification",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "prover",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "requestId",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "bool",
+          "name": "status",
+          "type": "bool"
+        }
+      ],
+      "name": "VerificationCompleted",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "verifier",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "prover",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "requestId",
+          "type": "uint256"
+        }
+      ],
+      "name": "VerificationRequested",
+      "type": "event"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_requestId",
+          "type": "uint256"
+        }
+      ],
+      "name": "getVerificationStatus",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "requestIdCounter",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "verifications",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "verifier",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "prover",
+          "type": "address"
+        },
+        {
+          "internalType": "bool",
+          "name": "completed",
+          "type": "bool"
+        },
+        {
+          "internalType": "bool",
+          "name": "status",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ]
+
+const contractAddress = '0xf63F2dc5634fAF75Fb9227B0ac0f5132BBE6A35c';
+
+
 
 const NewRequest = () => {
+const [contract, setContract] = useState(null);
+const [proverAddress, setProverAddress] = useState('');
   const [selectedOption, setSelectedOption] = useState('');
+  const [account, setAccount] = useState(null);
+
   const [minAge,setMinage]=useState('');
   const [maxAge,setMaxage]=useState('');
+  useEffect(() => {
+    const initBlockchain = async () => {
+      if (window.ethereum) {
+        const provider = new BrowserProvider(window.ethereum);
+  
+        // Request account access
+        const signer = await provider.getSigner();
+        console.log('address',signer.address)
 
+  
+        const contract = new Contract(contractAddress, contractABI, signer);
+        setContract(contract);
+        const userAddress = await signer.getAddress();
+        setAccount(userAddress);
+      } else {
+        console.log('Ethereum wallet not found');
+      }
+    };
+  
+    initBlockchain();
+  }, []);
+  const handleRequestVerification = async () => {
+    try {
+      console.log(proverAddress)
+      const tx = await contract.requestVerification(proverAddress);
+      const receipt = await tx.wait();
+      const event = receipt.events.find(event => event.event === 'VerificationRequested');
+      const requestId = event.args.requestId.toNumber();
+      console.log('Verification request sent:', requestId);
+    } catch (error) {
+      console.error('Error requesting verification:', error);
+    }
+  };
   const handleSelectChange = (e) => {
     e.preventDefault();
     setSelectedOption(e.target.value);
@@ -13,6 +210,7 @@ const NewRequest = () => {
   };
 const handleSubmit = (e)=>{
   e.preventDefault();
+handleRequestVerification()
 localStorage.setItem('minAge',minAge);
 localStorage.setItem('maxAge',maxAge);
 localStorage.setItem('category',selectedOption);
@@ -23,9 +221,22 @@ localStorage.setItem('category',selectedOption);
       <div className='lg:w-[400px] lg:h-[470px] border-[3px]
                        md:w-[400px] md:h-[450px]
                        w-[350px] h-[470px] border-[white]'>
+ <p>Connected Account: {account}</p>
+
         <div className='text-center mt-5 text-2xl font-bold'>Request Form</div>
         <form className='flex flex-col gap-4 text-black lg:ml-[20%] ml-[15%] mt-[10%]' onSubmit={handleSubmit}>
           <div>
+            
+          <label className="block text-sm font-medium text-[white]">Prover Address</label>
+              <input
+              id="textInput1"
+              type="text"
+              placeholder="Prover Address"
+              value={proverAddress}
+              onChange={(e) => setProverAddress(e.target.value)}
+
+              className="mt-1 block w-[250px] p-2 bg-gray-50 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+            />
             <label className="block text-sm font-medium text-[white]">
               Select an Option
             </label>
